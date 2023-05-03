d77de83a8e10bfa5f9e508fe55a96625d11893bc
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-05-03 14:01:48 -0400

    ci(GITHUB): add workflow name to notifications

--- .github/scripts/step-setup-environment.sh
+++ .github/scripts/step-setup-environment.sh
@@ -14,11 +14,11 @@
 # TEMPLATE_BRANCH_NAME_DEVELOPMENT: The name of the templated repository's development branch name.  (Defaults to 'dev'.)
 # TEST_PROJECT_NAME:                The slugified name of the template when populated during testing.  Should match any test GitHub repository in use.
 
-# 1:  A boolean value as a string, indicating if TESTING_MODE is active.
-
 # CI only script.
 
 set -eo pipefail
+
+WORKFLOW_NAME="${WORKFLOW_NAME:-""}"
 
 main() {
 
@@ -29,12 +29,12 @@ main() {
   BRANCH_OR_TAG="$(echo "${GITHUB_REF}" | sed -E 's,refs/heads/|refs/tags/,,g')"
   WORKFLOW_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
-  [[ "${1}" == "true" ]] && TESTING_CONTEXT="-tests"
+  [[ -n "${WORKFLOW_NAME}" ]] && WORKFLOW_NAME="-${WORKFLOW_NAME}"
 
   {
     echo "BRANCH_OR_TAG=${BRANCH_OR_TAG}"
     echo "CACHE_TTL=$(date +%d)"
-    echo "NOTIFICATION_LINK=${PROJECT_NAME}${TESTING_CONTEXT} [<${WORKFLOW_URL}|${BRANCH_OR_TAG}>]"
+    echo "NOTIFICATION_LINK=${PROJECT_NAME}${WORKFLOW_NAME} [<${WORKFLOW_URL}|${BRANCH_OR_TAG}>]"
     echo "PROJECT_NAME=${PROJECT_NAME}"
     echo "PROJECT_OWNER=${PROJECT_OWNER}"
     echo "TEMPLATE_BRANCH_NAME_BASE=${TEMPLATE_BRANCH_NAME_BASE-master}"
