c97ede93d1b0d5b5aca3e0dec1534ad690171928
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-05-18 08:02:58 -0400

    build(DOCKER): update container trufflehog version

---
+++ scripts/container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Build the CICD-tools container.
+
+# CICD-Tools Development script.
+
+set -eo pipefail
+
+# shellcheck source=./.cicd-tools/boxes/bootstrap/libraries/logging.sh
+source "$(dirname -- "${BASH_SOURCE[0]}")/../.cicd-tools/boxes/bootstrap/libraries/logging.sh"
+
+main() {
+  log "INFO" "Building the CICD-tools utility container ..."
+
+  pushd .cicd-tools/container >> /dev/null
+  docker build -t ghcr.io/niall-byrne/cicd-tools .
+  popd >> /dev/null
+
+  log "INFO" "Container successfully built."
+}
+
+main "$@"
