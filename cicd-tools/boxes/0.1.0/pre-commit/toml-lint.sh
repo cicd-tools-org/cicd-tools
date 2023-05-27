1ad4a947b189bfb0d8715c373d039cdcd1b2f9d7
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-05-27 11:38:04 -0400

    ci(PRE-COMMIT): simplify configuration

--- cicd-tools/boxes/0.1.0/pre-commit/toml-lint.sh
+++ cicd-tools/boxes/0.1.0/pre-commit/toml-lint.sh
@@ -10,18 +10,20 @@
 set -eo pipefail
 
 # shellcheck source=/dev/null
-source "$(dirname -- "${BASH_SOURCE[0]}")/../libraries/container.sh"
-
-# shellcheck source=/dev/null
 source "$(dirname -- "${BASH_SOURCE[0]}")/../libraries/logging.sh"
 
 main() {
+  local PRECOMMIT_TOMLL_DOCKER_IMAGE
+
+  PRECOMMIT_TOMLL_DOCKER_IMAGE="${1}"
+  shift
+
+  log "DEBUG" "PRE_COMMIT > Docker Image: '${PRECOMMIT_TOMLL_DOCKER_IMAGE}'"
+
   for TOML_FILE in "$@"; do
-
-    log "INFO" "PRE-COMMIT > tomll < ${TOML_FILE}"
-
-    diff "${TOML_FILE}" <(docker run -i --rm "$(container "get_image")" tomll < "${TOML_FILE}")
-
+    log "DEBUG" "PRE-COMMIT > tomll < ${TOML_FILE}"
+    diff "${TOML_FILE}" <(docker run -i --rm "${PRECOMMIT_TOMLL_DOCKER_IMAGE}" tomll < "${TOML_FILE}")
+    log "INFO" "PRE-COMMIT > ${TOML_FILE} passes!"
   done
 }
 
