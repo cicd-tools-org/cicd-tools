efae80528806ce4b0703a9ceed9a01f451a3047b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-12-17 10:23:43 -0500

    ci(GITHUB): support multiple container platforms

---
+++ scripts/containers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Build the CICD-Tools container.
+
+# CICD-Tools Development script.
+
+set -eo pipefail
+
+# shellcheck source=./.cicd-tools/boxes/bootstrap/libraries/logging.sh
+source "$(dirname -- "${BASH_SOURCE[0]}")/../.cicd-tools/boxes/bootstrap/libraries/logging.sh"
+
+main() {
+  log "INFO" "Building the CICD-Tools utility containers ..."
+
+  pushd .cicd-tools/container >> /dev/null
+  docker build \
+    --platform linux/amd64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_1=amd64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_2=x86_64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_3=x86_64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_4=64-bit \
+    -t ghcr.io/cicd-tools-org/cicd-tools:linux-amd .
+  docker build \
+    --platform linux/arm64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_1=arm64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_2=arm64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_3=aarch64 \
+    --build-arg BUILD_ARG_ARCH_FORMAT_4=arm64 \
+    -t ghcr.io/cicd-tools-org/cicd-tools:linux-amd .
+  popd >> /dev/null
+
+  log "INFO" "Containers successfully built."
+}
+
+main "$@"
