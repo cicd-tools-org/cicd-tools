7d483977a3dae6350a81b0ef94221de47e95e378
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-22 12:16:10 -0400

    feat(LOGGING): options to manage newlines

--- cicd-tools/boxes/0.1.0/libraries/logging.sh
+++ cicd-tools/boxes/0.1.0/libraries/logging.sh
@@ -11,9 +11,13 @@ source "$(dirname -- "${BASH_SOURCE[0]}")/colours.sh"
 LOGGING_LEVEL=${LOGGING_LEVEL-"DEBUG"}
 
 function log() {
-  # USAGE:
-  # colour bg [CRITICAL|ERROR|WARNING|INFO|DEBUG] [MESSAGE CONTENTS]
 
+  # USAGE:
+  # log (-b) (-n) [CRITICAL|ERROR|WARNING|INFO|DEBUG] [MESSAGE CONTENTS]
+
+  local LOGGING_MESSAGE_CONTENT
+  local LOGGING_MESSAGE_LEVEL
+  local LOGGING_NEW_LINE="\n"
   local LOGGING_SEVERITY_LEVELS=("DEBUG" "INFO" "WARNING" "ERROR" "CRITICAL")
 
   # shellcheck disable=SC2034
@@ -28,19 +32,48 @@ function log() {
   local DEBUG="CYAN"
   # shellcheck disable=SC2034
 
-  local LOGGING_MESSAGE_LEVEL="${1}"
-  local MESSAGE_CONTENT="${2}"
-
-  if [[ -z "${!LOGGING_LEVEL}" ]] ||
-    [[ -z "${MESSAGE_CONTENT}" ]]; then
-    log "ERROR" "Invalid logging statement!"
-    return 127
-  fi
+  _log_args "$@"
 
   if [[ "$(_log_get_severity_level "${LOGGING_MESSAGE_LEVEL}")" -ge "$(_log_get_severity_level "${LOGGING_LEVEL}")" ]]; then
-    echo "[$(date -u)] [$(colour fg "${!LOGGING_MESSAGE_LEVEL}")${LOGGING_MESSAGE_LEVEL}$(colour clear)] ${MESSAGE_CONTENT}" >> /dev/stderr
+    echo \
+      -e \
+      -n \
+      "[$(date -u)] [$(colour fg "${!LOGGING_MESSAGE_LEVEL}")${LOGGING_MESSAGE_LEVEL}$(colour clear)] ${LOGGING_MESSAGE_CONTENT}${LOGGING_NEW_LINE}" \
+      >> /dev/stderr
   fi
 
+}
+
+function _log_args() {
+  local OPTARG
+  local OPTIND
+  local OPTION
+
+  while getopts "bn" OPTION; do
+    case "$OPTION" in
+      b)
+        LOGGING_NEW_LINE="\n\n"
+        ;;
+      n)
+        LOGGING_NEW_LINE=""
+        ;;
+      \?)
+        _log_usage
+        ;;
+      *)
+        continue
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  LOGGING_MESSAGE_LEVEL="${1}"
+  LOGGING_MESSAGE_CONTENT="${2}"
+
+  if [[ -z "${!LOGGING_LEVEL}" ]] ||
+    [[ -z "${LOGGING_MESSAGE_CONTENT}" ]]; then
+    _log_usage
+  fi
 }
 
 function _log_get_severity_level() {
@@ -51,4 +84,15 @@ function _log_get_severity_level() {
       echo "${LOGGIN_SEVERITY_LEVEL}"
     fi
   done
+}
+
+function _log_usage() {
+  log "ERROR" "log -- a function to write logging messages to the console."
+  log "ERROR" "-----------------------------------------------------------"
+  log "ERROR" "log"
+  log "ERROR" "          -b (OPTIONAL SWITCH TO ADD AN EXTRA TRAILING NEWLINE.)"
+  log "ERROR" "          -n (OPTIONAL SWITCH TO DISABLE TRAILING NEWLINES.)"
+  log "ERROR" "          [LEVEL]       [CRITICAL|ERROR|WARNING|INFO|DEBUG]"
+  log "ERROR" "          [MESSAGE]     [THE LOGGING MESSAGE TO WRITE]"
+  return 127
 }
