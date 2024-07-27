94ec3d607df8e35427c154a12b36ee3db49f353e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-07-26 20:00:19 -0400

    feat(CONTAINER): add gpg container

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -10,9 +10,9 @@ set -eo pipefail
 source "$(dirname -- "${BASH_SOURCE[0]}")/../.cicd-tools/boxes/bootstrap/libraries/logging.sh"
 
 main() {
-  log "INFO" "Building the CICD-Tools utility containers ..."
+  log "INFO" "Building the CICD-Tools utility container ..."
 
-  pushd .cicd-tools/container >> /dev/null
+  pushd .cicd-tools/containers/utilities >> /dev/null
   log "INFO" "Building AMD64 ..."
   docker build \
     --no-cache \
@@ -31,6 +31,21 @@ main() {
     --build-arg BUILD_ARG_ARCH_FORMAT_3=aarch64 \
     --build-arg BUILD_ARG_ARCH_FORMAT_4=arm64 \
     -t ghcr.io/cicd-tools-org/cicd-tools:linux-arm .
+  popd >> /dev/null
+
+  log "INFO" "Building the CICD-Tools gpg container ..."
+
+  pushd .cicd-tools/containers/gpg >> /dev/null
+  log "INFO" "  Building AMD64 ..."
+  docker build \
+    --no-cache \
+    --platform linux/amd64 \
+    -t ghcr.io/cicd-tools-org/cicd-tools-gpg:linux-amd .
+  log "INFO" "  Building ARM64 ..."
+  docker build \
+    --no-cache \
+    --platform linux/arm64 \
+    -t ghcr.io/cicd-tools-org/cicd-tools-gpg:linux-arm .
   popd >> /dev/null
 
   log "INFO" "Containers successfully built."
