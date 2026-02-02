569605da00cbdae9d6f270efbbd861a1cfff9bd9
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-02 07:28:36 -0500

    feat(CONTAINERS): prefer debian security repository

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 main() {
   log "INFO" "Building the CICD-Tools utility container ..."
 
-  pushd .cicd-tools/containers/utilities >> /dev/null
+  pushd .cicd-tools/containers >> /dev/null
   log "INFO" "Building AMD64 ..."
   docker build \
     --no-cache \
@@ -18,6 +18,7 @@ main() {
     --build-arg BUILD_ARG_ARCH_FORMAT_2=x86_64 \
     --build-arg BUILD_ARG_ARCH_FORMAT_3=x86_64 \
     --build-arg BUILD_ARG_ARCH_FORMAT_4=64-bit \
+    -f utilities/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools:linux-amd .
   log "INFO" "Building ARM64 ..."
   docker build \
@@ -27,36 +28,41 @@ main() {
     --build-arg BUILD_ARG_ARCH_FORMAT_2=arm64 \
     --build-arg BUILD_ARG_ARCH_FORMAT_3=aarch64 \
     --build-arg BUILD_ARG_ARCH_FORMAT_4=arm64 \
+    -f utilities/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools:linux-arm .
   popd >> /dev/null
 
   log "INFO" "Building the CICD-Tools gettext container ..."
 
-  pushd .cicd-tools/containers/gettext >> /dev/null
+  pushd .cicd-tools/containers >> /dev/null
   log "INFO" "  Building AMD64 ..."
   docker build \
     --no-cache \
     --platform linux/amd64 \
+    -f gettext/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools-gettext:linux-amd .
   log "INFO" "  Building ARM64 ..."
   docker build \
     --no-cache \
     --platform linux/arm64 \
+    -f gettext/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools-gettext:linux-arm .
   popd >> /dev/null
 
   log "INFO" "Building the CICD-Tools gpg container ..."
 
-  pushd .cicd-tools/containers/gpg >> /dev/null
+  pushd .cicd-tools/containers >> /dev/null
   log "INFO" "  Building AMD64 ..."
   docker build \
     --no-cache \
     --platform linux/amd64 \
+    -f gpg/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools-gpg:linux-amd .
   log "INFO" "  Building ARM64 ..."
   docker build \
     --no-cache \
     --platform linux/arm64 \
+    -f gpg/Dockerfile \
     -t ghcr.io/cicd-tools-org/cicd-tools-gpg:linux-arm .
   popd >> /dev/null
 
